c0da18cf4c446311d2ed832b90cbb921250a1c3c
Ian Wahbe <ian@wahbe.com>
2020-11-16 02:08:25 +0100

    Tests should be pretty

--- testing.sh
+++ testing.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
 
+# Compile and unit tests
 cargo test || exit
 cargo build --release || exit
 
+# find executable and test directory
 test_dir=tests
 find=$(realpath ./target/release/find)
 original_dir=$PWD
 
-echo -e "\nIntegration Tests"
+all_passed=true
+fail () {
+    echo -e "\e[31mfailed\e[39m"
+    all_passed=false
+}
+pass () {
+    echo -e "\e[32mok\e[39m"
+}
+
+# run tests
+echo -e "\n\n\t\e[1m\e[32m Start Integration Tests\e[0m\n"
 for tst in $test_dir/*; do
     cd $tst || exit
     args=$(cat args)
@@ -16,9 +28,13 @@ for tst in $test_dir/*; do
     echo -n "Testing $tst with arg: $args ... "
     diff test.out crct.out > /dev/null && \
         diff test.err crct.err > /dev/null && \
-        echo -e "\e[32mok\e[39m" || \
-        echo -e "\e[31mfailed\e[39m"
+        pass || fail
     cd $original_dir
 done
 
-cd $original_dir
+if [ "$all_passed" = true ]; then
+    color="\e[32m"
+else
+    color="\e[31m"
+fi
+echo -e "\n\t$color\e[1mFinished Integration Tests\e[39m\n"
